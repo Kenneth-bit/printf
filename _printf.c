2cb308b793dc6fae9ad12acd30947b897137834d
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-07-26 14:19:41 -0700

    Add logic code

--- _printf.c
+++ _printf.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
 int _printf(char *format, ...)
 {
-	(void) format;
-	return (0);
+	va_list args;
+	int res;
+
+	va_start (args, format);
+	res = vfprintf (stdout, format, args);
+	va_end (args);
+	return (res);
 }
