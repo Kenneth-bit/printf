37c1f044088184e7d2d657a67eab6f355317ba30
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-07-26 14:32:02 -0700

    Create holberton.h

--- holberton.h
+++ holberton.h
@@ -2,4 +2,8 @@
   * _printf - produces output according to format
   * Return - number of characters printed (excluding null byte)
   */
+#ifndef _PRINTF_H
+#define _PRINTF_H
 int _printf(const char *format, ...);
+
+#endif
